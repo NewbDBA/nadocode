8f97b024c5998a54c0c1dc088ee48409d37b354c
forte <forte3x@gmail.com>
2021-08-06 17:05:41 +0900

    21.08.06_EOB

--- practice.py
+++ practice.py
@@ -12,3 +12,29 @@ print((3 < 0) or (3 > 5)) # 둘 중에 하나만 되도 okay
 print((3 < 0) | (3 > 5)) # 위에 거랑 같은데, 백스페이스 밑에꺼 shift +x
 
 # 연산자
+
+# 간단수식
+number = 2+4*3
+number = number + 2 #number라는 변수에 2 더하고 싶댑니다.#
+print(number)
+
+# 아래거는 위에꺼랑 똑같음
+number += 2 # *= 숫자 // -= 숫자 // /=숫자 다됨
+print(number)
+
+
+asshole = 43+5+3*8
+asshole %= 5
+print(asshole)
+# 간단수식
+
+# 숫자 처리 함수 
+print(abs(-5)) # ABS=absolute (절대값)
+print(pow(5, 2)) # == 5 ** 2
+print(max(5,10,12)) # min 도 있음
+print(round(3.2)) # 반올림
+
+from math import * # math 라는 모듈에서 모든 값(*)을 불러온다는 말
+print(floor(5.99)) # math 모둘 호출 후에 버림(floor) 가능
+print(ceil(1.11)) # 올림
+print(sqrt(4)) # 제곱근
